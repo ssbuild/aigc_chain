3bf957aa0b26d8cc6f2b1974eac6efe020200900
tk <462304@qq.com>
2023-10-31 02:27:14 +0800

    update
    Signed-off-by: tk <462304@qq.com>

--- tests/test_openai_chat2.py
+++ tests/test_openai_chat2.py
@@ -4,13 +4,10 @@ import openai
 # 新版本
 openai.api_key = "EMPTY"
 openai.api_base = "http://192.168.2.180:8081/v1"
-
-openai.proxy = None
 # model = "chatglm2-6b-int4"
 # model = "qwen-7b-chat-int4"
-# model = "ChatYuan-large-v2"
-model = "qwen-7b-chat"
-
+# model = "qwen-7b-chat"
+model = "chatglm3-6b"
 # # Test list models API
 # models = openai.Model.list()
 # print("Models:", models)
@@ -18,7 +15,7 @@ model = "qwen-7b-chat"
 stream = False
 
 input_text ="以下是中国关于刑事诉讼法考试的单项选择题，请选出其中的正确答案。\n\n某市人民检察院在侦查该市财政局副局长张某受贿案的过程中，除发现张某利用职权之便收受他人贿赂之外，还发现张某涉嫌巨额财产来源不明罪和非法向外国人出售、赠送珍贵文物罪。指控张某犯有巨额财产来源不明罪，首先应当承担证明其财产或者支出明显超过其合法收入且差额巨大的证明责任的是下列哪个机关或人员?\nA. 检察院报人\nC. 张某本人\nD. 财政局\n答案："
-
+input_text = "你是谁"
 data = {
     "model": model,
     "adapter_name": None, # lora头
